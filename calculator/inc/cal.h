f2b4e9aa273b580759f2efe7514e6db338aad755
naveenprakash210 <69070247+naveenprakash210@users.noreply.github.com>
2020-09-27 20:37:27 +0530

    Add files via upload

---
+++ calculator/inc/cal.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void display(float n1, float n2, char ch, float result);
+void add(float n1, float n2);
+void subtract(float n1, float n2);
+void multiply(float n1, float n2);
+void divide(float n1, float n2);
+void rem(float n1, float n2);
+void power(float n1, float n2);
+
---
+++ calculator/test/test_cal.c
@@ -0,0 +1,14 @@
+#include "unity.h"
+
+//Required by the test unity framework
+void setUp() {}
+
+//Required by the unity framework
+void tearDown() {}
+
+void testing_pallindrome_number(void)
+{
+    TEST_ASSERT_EQUAL(2, add(2,2));
+    TEST_ASSERT_EQUAL(2, substract(4,2));
+
+}
